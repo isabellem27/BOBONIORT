a06bad690f10861b9157af10cb39fc43584348bd
Alexandre <146923992+AlexEnCode@users.noreply.github.com>
2024-06-14 18:21:18 +0200

    Update rptmenu.cbl

--- RAPPORT/MENU-RAPPORT/rptmenu.cbl
+++ RAPPORT/MENU-RAPPORT/rptmenu.cbl
@@ -1,1 +1,57 @@
+      ****************************************************************** 
 
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. rptmenu RECURSIVE.
+       AUTHOR. Alexandre&.
+
+      ******************************************************************
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01  WS-ERROR-MESSAGE     PIC X(70).
+       01  WS-UPDATE-VALIDATION PIC X(01).
+       01  WS-MENU-RETURN       PIC X(01).
+       01  WS-MAIL-AROBASE      PIC 9(01) VALUE 0.
+
+       LINKAGE SECTION.
+       01 LK-CUSTOMER.
+           03 LK-CUS-UUID        PIC X(36).
+           03 LK-CUS-GENDER      PIC X(10).
+           03 LK-CUS-LASTNAME    PIC X(20).
+           03 LK-CUS-FIRSTNAME   PIC X(20).
+           03 LK-CUS-ADRESS1	 PIC X(50).
+           03 LK-CUS-ADRESS2	 PIC X(50).
+           03 LK-CUS-ZIPCODE	 PIC X(15).
+           03 LK-CUS-TOWN	     PIC X(50).
+           03 LK-CUS-COUNTRY	 PIC X(20).
+           03 LK-CUS-PHONE	     PIC X(10).
+           03 LK-CUS-MAIL	     PIC X(50).
+           03 LK-CUS-BIRTH-DATE.
+               05 LK-YEAR        PIC X(04).
+               05 LK-SEPARATOR1  PIC X(01).
+               05 LK-MONTH       PIC X(02).
+               05 LK-SEPARATOR2  PIC X(01).
+               05 LK-DAY         PIC X(02).
+           03 LK-CUS-DOCTOR	     PIC X(50).
+           03 LK-CUS-CODE-SECU.
+               05 LK-SECU-1      PIC X(01).
+               05 LK-SECU-2      PIC X(02).
+               05 LK-SECU-3      PIC X(02).
+               05 LK-SECU-4      PIC X(02).
+               05 LK-SECU-5      PIC X(03).
+               05 LK-SECU-6      PIC X(03).
+               05 LK-SECU-7      PIC X(02).
+           03 LK-CUS-CODE-IBAN   PIC X(34).
+           03 LK-CUS-NBCHILDREN  PIC 9(03).
+           03 LK-CUS-COUPLE      PIC X(05).
+           03 LK-CUS-CREATE-DATE PIC X(10).
+           03 LK-CUS-UPDATE-DATE PIC X(10).
+           03 LK-CUS-CLOSE-DATE  PIC X(10).
+           03 LK-CUS-ACTIVE	     PIC X(01).
+
+       SCREEN SECTION.
+       COPY ' '.
+
+      ******************************************************************
+
+       PROCEDURE DIVISION USING LK-CUSTOMER.
