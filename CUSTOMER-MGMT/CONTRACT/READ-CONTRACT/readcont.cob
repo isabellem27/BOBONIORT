6544848ff79389286e671e5793d80a4730d82f57
Remi-Debu <debu.remi@gmail.com>
2024-06-24 11:24:18 +0200

    bugfix

--- CUSTOMER-MGMT/CONTRACT/READ-CONTRACT/readcont.cob
+++ CUSTOMER-MGMT/CONTRACT/READ-CONTRACT/readcont.cob
@@ -418,7 +418,7 @@ OCESQL    .
       ****************************************************************** 
        5000-START-SCREEN.
            IF WS-COUNT-CUS-REIM GREATER THAN 1 THEN
-               MOVE 'Plusieurs contrats affectes pour cette adherent.' 
+               MOVE 'Plusieurs contrats affectes pour cet adherent.' 
                TO LK-ERROR-MESSAGE-MENU
 
                CALL 
@@ -427,7 +427,7 @@ OCESQL    .
                    LK-CUSTOMER
                END-CALL
            ELSE IF WS-COUNT-CUS-REIM LESS THAN 1 THEN
-               MOVE 'Aucun contrat affecte pour cette adherent.' 
+               MOVE 'Aucun contrat affecte pour cet adherent.' 
                TO LK-ERROR-MESSAGE-MENU
 
                CALL 
